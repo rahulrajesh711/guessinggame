65a830e7558a7b9590360d3a13c365e836182b6a
rahulrajesh711 <rahulrajesh711@gmail.com>
2020-04-16 01:49:44 +0530

    Final changes

--- guessinggame.sh
+++ guessinggame.sh
@@ -3,8 +3,8 @@
 
 count=$(ls -1 | wc -l)
 
-function correct_guess {        # One function...
-    if [[ $1 -lt $2 ]]          # One if statement ...
+function correct_guess {
+    if [[ $1 -lt $2 ]]
     then
 	echo "Too low!"
     elif [[ $1 -gt $2 ]]
@@ -16,7 +16,7 @@ function correct_guess {        # One function...
     fi
 }
 
-while [[ $count -ne $guess ]]    # One loop...
+while [[ $count -ne $guess ]]
 do
     read -p "Guess how many files are in the current directory? " guess
     # echo "You entered: $guess"
